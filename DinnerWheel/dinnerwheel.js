a487f54c97707c01b05c7b47d0ca43e80d5f878a
Chloe Hsu <vivi831018@hotmail.com>
2017-03-09 00:11:01 +0000

    working on spinning wheel

---
+++ DinnerWheel/dinnerwheel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('.wrapper').fadeIn('slow', function(){
+            var rot = Math.floor(Math.random() * 360);
+            console.log('rot', rot);
+            var num = rot + 5000;
+            var value = 'rotate(' + num + 'deg)';
+            $('.spinning-wheel').css('transform', value);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           });
+    }) 
+})
