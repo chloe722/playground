0676f0465e8e9dac6b5d1192721eb3e58f3b7f90
Chloe Hsu <vivi831018@hotmail.com>
2017-03-10 23:38:52 +0000

    Added button and Arrow

--- DinnerWheel/dinnerwheel.js
+++ DinnerWheel/dinnerwheel.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $('button').click(function(){
-        $('.wrapper').fadeIn('slow', function(){
+        $('.wrapper').slideDown('slow', function(){
             var rot = Math.floor(Math.random() * 360);
             console.log('rot', rot);
             var num = rot + 5000;
             var value = 'rotate(' + num + 'deg)';
-            $('.spinning-wheel').css('transform', value);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           });
+            $('.spinning-wheel').css('transform', value);   
+            $('.Arrow').fadeIn('slow');                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        });
     }) 
 })
