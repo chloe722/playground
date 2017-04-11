8cecbf7f9e9c0636e57961d8a17d8213ea84e78d
Chloe Hsu <vivi831018@hotmail.com>
2017-04-11 00:19:09 +0000

    Improved and added most of features
    -Improved the dialog position
    -Improved the radom graphic of gesture displayed
    -Improved heading and title colors
    -Added 'START' and 'Again' buttons 
    -Added user and player characters's movement 
    -Added cinematic graphic for each result

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -4,40 +4,64 @@ function rockPaperScissor(userInput) {
 
         computerChoose(computerChoice);
         userChoose(userInput);
+        clearInterval(showRandomInterval);
 
         if (userInput === 'ROCK' ) {
             if (computerChoice === 'ROCK') {
                 dialogText('Rock to rock...Seems we need to fight more to see who is winner');
+                $('.rockTorock').show(1000);
+            
             } 
             else if (computerChoice === 'PAPER') {
                 dialogText('My paper wrapped your rock!..HaHaHa!');
+                $('.rockIswrapped').show(1000);
+            
             }
             else if (computerChoice === 'SCISSOR') {
                 dialogText('Oh no... your rock is too strong..please show some mercy!');
+                $('.rockBeatscissor').show(1000);
+            
             }
         }
         if (userInput === 'PAPER') {
             if (computerChoice === 'ROCK') {
                 dialogText('I am struggling to make my rock to run away from your paper..');
+                $('.paperAndpaper').show(1000);
+            
             }
             else if (computerChoice === 'PAPER') {
                 dialogText('Two papers....what we can do with two papers...? Cannot even fight!');
+                $('.paperWraprock').show(1000);
+            
             }
             else if(computerChoice === 'SCISSOR') {
                 dialogText('Scissor..Scissor..I am going to cut cut cut!');
+                $('.scissorAttack').show(1000);               
             }
+            setTimeout(function(){
+                    $('.restart').show();
+                },1000);
         }
         if (userInput === 'SCISSOR') {
             if (computerChoice === 'ROCK') {
                 dialogText('My rock is much stronger than your scissor! What you gonna do ?');
+                $('.rockCrashuser').show(1000);
+            
+                
             }
             else if (computerChoice === 'PAPER') {
                 dialogText('No No No! Please do not cut my paper');
+                $('.beggingCat').show(1000);
+            
             }
             else if (computerChoice === 'SCISSOR') {
                 dialogText('Both of us are scissor..How about be my friend? PEACE!');
+                $('.friend').show(1000);    
             }
         }
+        setTimeout(function(){
+            $('.restart').show();
+        }, 2000);
 }
 
 
@@ -68,7 +92,7 @@ function computerChoose(choice) {
 }
 
 function dialogText(text){
-    $('#dialog').text(text).show();
+    $('#dialog').text(text).show(1000);
 }
 
 function showRandom(){
@@ -79,23 +103,40 @@ function showRandom(){
     userChoose  (humanChoice);
 }
 
-setTimeout(function(){
-    $('.cat').css({
-        'top' :'100px',
-        'left': '300px'
-    })
-}, 1000);
+$('document').ready(function(){
+        $('.cat,.boy').hide(500);
+        $('.cat2,.boy2').show(1000);
+        setTimeout(function(){$('.cat2,.boy2').hide();}, 900);
+        $('.cat3,.boy3').show(2000);
 
-setTimeout(function(){
-    $('.cat').css({
-        'top': '300px',
-        'left': '300px'
-    })
-}, 2000);
+        $('.restart').click(againClick);
+        setTimeout(function(){
+            $('.start').show();
+        }, 3000);
+        $('.start').click(function(){
+            setTimeout(function(){
+                $('.BUTTON').show(1000);
+            }, 2000);
+            $('.start').hide();
+            $('.graphic').fadeOut();
+            window.showRandomInterval = setInterval(showRandom, 90);
+        });
+        
+});
 
-setTimeout(function(){
-    $('.cat').css({
-        'top':'200px',
-        'left':'200px'
-    })
-}, 3000);
+function againClick(){ 
+   $('.cinematic').hide(100);
+   $('#dialog').hide(100);
+   $('.restart').hide();
+   window.showRandomInterval = setInterval(showRandom, 80);
+}
+
+/*window.onload = function(){
+    var hoverSound = document.getElementById('hoverSound');
+    var start = document.getElementsByClassName('start');
+
+    start.onmousehover=function(){
+        hoverSound.play();
+        return false;
+    };
+}; */
