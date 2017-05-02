71737ad318407d18d6f321a1ccedbd788575c09e
Chloe Hsu <vivi831018@hotmail.com>
2017-05-03 00:36:30 +0100

    Created form project
    -Added two question 
    -Displayed in radio type
    -Displayed in drop down box menu
    -Button to switch between line-style and grid-style

---
+++ QuestionForm/QuestionForm.js
@@ -0,0 +1,7 @@
+function gridStyle() {
+    $("body").removeClass('line-style').addClass('grid-style');
+}
+
+function lineStyle() {
+    $("body").removeClass('grid-style').addClass('line-style');
+}
