69a14ddf56f754401486f9ca00cd219420a38fe7
Chloe Hsu <vivi831018@hotmail.com>
2017-05-19 00:27:59 +0100

    Added new picture, improved the code for timing in JS, CSS

--- DigitalClock/digitalClock.js
+++ DigitalClock/digitalClock.js
@@ -15,91 +15,65 @@
     return val_array[0] + "<br/>" + val_array[1]
 }
 */
-function Alaskaupdate(){
-    var clock = document.getElementById('alaskaTime');
-    var now = moment();
-    AlaskaTime = now.tz("America/Anchorage").format('MMMM Do YYYY')+'<br/>'+ now.format('h:mm:ss a');
-    clock.innerHTML = AlaskaTime;
+
+function Timeupdate(ElementId, TimeZone, PicId, newClass){
+    var clock = document.getElementById(ElementId);
+    var now = moment().tz(TimeZone);
+    clock.innerHTML = now.format("MMMM Do YYYY") + '<br/>' + now.format('HH:mm:ss');
+    if (now.hours() >= 18 || now.hours()<=5) {
+        $(PicId).addClass(newClass);
+    }
 }
 
-function LosAngeleskupdate(){
-    var clock = document.getElementById('losangelesTime');
-    var now = moment();
-    LosAngelesTime = now.tz("America/Los_Angeles").format('MMMM Do YYYY')+'<br/>'+ now.format('h:mm:ss a');
-    clock.innerHTML = LosAngelesTime;
-}
 
-function NewYorkupdate(){
-    var clock = document.getElementById('newyorkTime');
-    var now = moment();
-    NewYorkTime = now.tz("America/New_York").format("MMMM Do YYYY")+'<br/>'+ now.format('h:mm:ss a');
-    clock.innerHTML = NewYorkTime;
-}
-
-function Londonupdate(){
+/*function Londonupdate(){
     var clock = document.getElementById('londonTime');
     var now = moment();
     LondonTime = now.format('MMMM Do YYYY') +'<br/>'+ now.format('h:mm:ss a');
     clock.innerHTML = LondonTime;
     
-}
+}*/
 
-function Berlinupdate(){
+/*function Berlinupdate(){
     var clock = document.getElementById('berlinTime');
     var now = moment();
     var Time = now.tz("Europe/Berlin")
     var BerlinTime = Time.format("MMMM Do YYYY") + '<br/>' + Time.format('HH:mm:ss ');
     clock.innerHTML = BerlinTime;
-
     if (Time.hours() >= 18 || Time.hours()<=5) {
         $('#BerlinPic').addClass('berlinNight');
     }
+}*/
+
+
+
+
+Timeupdate("alaskaTime" , "America/Anchorage","#AlaskaPic","alaskaNight" );
+Timeupdate("newyorkTime" , "America/New_York","#NewYorkPic","newyorkNight");
+Timeupdate("losangelesTime" , "America/Los_Angeles","#LAPic","LANight");
+Timeupdate("londonTime" , "Europe/London","#LondonPic","londonNight");
+Timeupdate("berlinTime", "Europe/Berlin", '#BerlinPic','berlinNight');
+Timeupdate("vilniusTime" , "Europe/Vilnius","#VilniusPic","vilniusNight");
+Timeupdate("taipeiTime" , "Asia/Taipei","#TaipeiPic","taipeiNight");
+Timeupdate("tokyoTime" , "Asia/Tokyo","#TokyoPic","tokyoNight");
+Timeupdate("sydneyTime" , "Australia/Sydney","#SydneyPic","sydneyNight");
+
+
+function UpdateAll(){
+    Timeupdate("alaskaTime" , "America/Anchorage","#AlaskaPic","alaskaNight" );
+    Timeupdate("newyorkTime" , "America/New_York","#NewYorkPic","newyorkNight");
+    Timeupdate("losangelesTime" , "America/Los_Angeles","#LAPic","LANight");
+    Timeupdate("londonTime" , "Europe/London","#LondonPic","londonNight");
+    Timeupdate("berlinTime", "Europe/Berlin", '#BerlinPic','berlinNight');
+    Timeupdate("vilniusTime" , "Europe/Vilnius","#VilniusPic","vilniusNight");
+    Timeupdate("taipeiTime" , "Asia/Taipei","#TaipeiPic","taipeiNight");
+    Timeupdate("tokyoTime" , "Asia/Tokyo","#TokyoPic","tokyoNight");
+    Timeupdate("sydneyTime" , "Australia/Sydney","#SydneyPic","sydneyNight");
 }
 
-
-
-function Vilniusupdate(){
-    var clock = document.getElementById('vilniusTime');
-    var now = moment();
-    VilniusTime = now.tz("Europe/Vilnius").format("MMMM Do YYYY") + '<br/>' + now.format('h:mm:ss a');
-    clock.innerHTML = VilniusTime;
-    
-}
-
-function Taipeiupdate(){
-    var clock = document.getElementById('taipeiTime');
-    var now = moment();
-    TaipeiTime = now.tz("Asia/Taipei").format("MMMM Do YYYY") + '<br/>' + now.format('h:mm:ss a');
-    clock.innerHTML = TaipeiTime;
-}
-
-function Tokyoupdate(){
-    var clock = document.getElementById('tokyoTime');
-    var now = moment();
-    TokyoTime = now.tz("Asia/Tokyo").format("MMMM Do YYYY") + '<br/>' + now.format('h:mm:ss a');
-    clock.innerHTML = TokyoTime;
-}
-
-
-
-function Sydneyupdate(){
-    var clock = document.getElementById('sydneyTime');
-    var now = moment();
-    SydneyTime = now.tz("Australia/Sydney").format("MMMM Do YYYY") + '<br/>' + now.format('h:mm:ss a');
-    clock.innerHTML = SydneyTime;
-
-}
 
 window.addEventListener("load", function(e){
-    setInterval(Alaskaupdate,1000);//setInterval makes clock 'tick' repeatedly
-    setInterval(LosAngeleskupdate,1000);
-    setInterval(NewYorkupdate,1000);
-    setInterval(Londonupdate,1000);
-    setInterval(Berlinupdate,1000);
-    setInterval(Vilniusupdate,1000);
-    setInterval(Taipeiupdate,1000);
-    setInterval(Tokyoupdate,1000); 
-    setInterval(Sydneyupdate,1000);
+    setInterval(UpdateAll,1000); //setInterval makes clock 'tick' repeatedly
 })
 
 
